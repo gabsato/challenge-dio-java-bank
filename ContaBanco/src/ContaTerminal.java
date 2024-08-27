a458988c2e379f481ccc00ae476fee589c8fee76
Gabriel A. Sato <gwork1324@gmail.com>
2024-08-27 18:27:46 -0300

    desafio banco em terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite a sua agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o número da conta");
+        int numero = scanner.nextInt();
+
+        System.out.println("Saldo da conta: ");
+        int saldo = scanner.nextInt();
+
+        System.out.println("Olá " + nome + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
